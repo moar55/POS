307f4c36fe7e36f4559164ab5bd40373dcc80cd9
Abdurrrahman <abdurrahman.maged@gmail.com>
2018-01-16 01:18:03 +0200

    finished login service (couldn't log in tho) ... still trying to navigate through the code but there is a fuckin issue i can't find a solution for ... generated the stock component

--- client/ngPOS/src/app/login.service.ts
+++ client/ngPOS/src/app/login.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClient } from '@angular/common/http';
+// import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class LoginService {
 
   constructor(public http: HttpClient) { }
 
-
-  login (username,password) {
-    // this.http.
+  baseUrl = 'http://127.0.0.1:3333';
+  login (loginData) {
+    return this.http.post(`${this.baseUrl}/api/login`, JSON.stringify(loginData));
 
   }
 
--- client/ngPOS/src/app/login/login.component.ts
+++ client/ngPOS/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
-import { LoginService } from './../login.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from './../login.service';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loggedIn = true;
+  loginData = {
+    userName: '',
+    passWord: '',
+  };
 
-  constructor(public login: LoginService) { }
+  constructor(public login: LoginService, public router: Router) { }
 
   ngOnInit() {
+  }
+  submit(loginData) {
+    this.login.login(this.loginData).subscribe( (res) => {
+      console.log(res);
+      this.router.navigate(['stock']);
+  },
+  err => {console.log(err); }
+    );
+
   }
 }
