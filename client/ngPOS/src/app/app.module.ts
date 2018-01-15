84bf7091fbf7f3dd6aa1c8aa3d83b559d7991f84
Abdurrrahman <abdurrahman.maged@gmail.com>
2018-01-16 01:18:03 +0200

    finished login service (couldn't log in tho) ... still trying to navigate through the code but there is a fuckin issue i can't find a solution for ... generated the stock component

--- client/ngPOS/src/app/app.module.ts
+++ client/ngPOS/src/app/app.module.ts
@@ -1,21 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LoginService } from './login.service';
+import { HttpClientModule } from '@angular/common/http';
+import { StockComponent } from './stock/stock.component';
 
+const appRoutes: Routes = [
+  // { path: '/stock', component: StockComponent },
+  // { path: 'hero/:id', component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    StockComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [LoginService],
+  providers: [
+    LoginService,
+    Router
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
