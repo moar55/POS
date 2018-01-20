1e2f8433ebdc89e12153a39d3001e621c1deddf7
Omar Hossam <moar.ahmed@gmail.com>
2018-01-20 23:44:26 +0200

    .

--- config/database.js
+++ config/database.js
@@ -3,6 +3,8 @@
 const Env = use('Env')
 const Helpers = use('Helpers')
 const { URL } = require('url');
+const JAWSDB_URL = new Url(Env.get('JAWSDB_URL'))
+
 
 module.exports = {
   /*
@@ -48,11 +50,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'POS')
+      host: Env.get('DB_HOST', JAWSDB_URL.host),
+      port: Env.get('DB_PORT', JAWSDB_URL.port),
+      user: Env.get('DB_USER', JAWSDB_URL.username),
+      password: Env.get('DB_PASSWORD', JAWSDB_URL.password),
+      database: Env.get('DB_DATABASE', JAWSDB_URL.pathname.substr(1))
     }
   },
 
