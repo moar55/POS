86520bb9b3cc0c42e9ad8cddc1a3e7a2b627acaf
Omar Hossam <moar.ahmed@gmail.com>
2018-02-10 16:12:11 +0200

    .

--- client/ngPOS/main.js
+++ client/ngPOS/main.js
@@ -4,7 +4,7 @@ let win;
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 600, 
+    width: 600,
     height: 600,
     backgroundColor: '#ffffff',
     icon: `file://${__dirname}/dist/assets/logo.png`  //put the app icon here
